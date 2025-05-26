bf2481724738bed5ce4a298881030bd8f35de8bf
Macnelson9 <michaelofatu@gmail.com>
2025-05-26 21:15:35 +0100

    Learnt about Types

---
+++ sandbox.js
@@ -0,0 +1,10 @@
+var character = 'uche';
+var age = 25;
+var isBlackBelt = false;
+// We can change the values of the types defined above but not the types themselves.
+// character = 20;
+character = 'ofatu';
+// age = 'uche';
+age = 24;
+// isBlackBelt = 'yes';
+isBlackBelt = true;
---
+++ sandbox.ts
@@ -0,0 +1,13 @@
+let character = 'uche';
+let age = 25;
+let isBlackBelt = false;
+
+// We can change the values of the types defined above but not the types themselves.
+// character = 20;
+character = 'ofatu';
+
+// age = 'uche';
+age = 24;
+
+// isBlackBelt = 'yes';
+isBlackBelt = true;
